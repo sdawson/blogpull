418b4944a22cbe8f2ebd3a2c73539803ab3593b6
Sophie Dawson <sophiecdawson@gmail.com>
2012-02-01 18:56:37 +1100

    Cleanup.

--- blogpull.py
+++ blogpull.py
@@ -3,7 +3,7 @@ import gdata
 import atom
 import sys, time, os, errno
 
-def runningPostTitlePrint(blogger_service, uri, dirname):
+def pullFeed(blogger_service, uri, dirname):
   # Only create the requested directory if it doesn't already exist
   try:
     os.makedirs(dirname)
@@ -40,23 +40,14 @@ def saveToFile(entry, dirname):
 def usage():
   print "Usage: python blogpull.py outputdirname"
 
-def getUrl(filename):
-  f = open(filename, 'r')
-  url = f.readline().rstrip()
-  f.close
-  return ''.join([url, "/feeds/posts/default"])
-
 def main():
   print len(sys.argv[1:])
   if len(sys.argv[1:]) != 2:
     usage()
     sys.exit(1)
   blogger_service = service.GDataService()
-  #PrintAllPosts(blogger_service)
-  print "argv[1]: ", sys.argv[1]
-  url = getUrl(sys.argv[1])
   outputfile = sys.argv[2]
-  runningPostTitlePrint(blogger_service, url, outputfile)
+  pullFeed(blogger_service, url, outputfile)
 
 if __name__ == '__main__':
   main()

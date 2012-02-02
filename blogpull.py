735656b201b0f583b8170a7e781844e80ce45d0e
Sophie Dawson <sophiecdawson@gmail.com>
2012-02-02 13:25:44 +1100

    Downloading images locally.
    Doesn't download to to user-specified download folder.

--- blogpull.py
+++ blogpull.py
@@ -1,9 +1,10 @@
 from gdata import service
 from BeautifulSoup import BeautifulStoneSoup
-from urlparse import urlparse
-import gdata
-import atom
-import sys, time, os, errno
+from urlparse import urlparse, urlsplit
+from os.path import basename
+import gdata, atom, mimetypes
+import sys, time, os, errno, urllib
+import urllib2
 
 def pullFeed(blogger_service, uri, dirname):
   # Only create the requested directory if it doesn't already exist
@@ -14,13 +15,14 @@ def pullFeed(blogger_service, uri, dirname):
       raise
 
   feed = blogger_service.GetFeed(uri)
-  for entry in feed.entry:
-    saveToFile(entry, dirname)
-  feed = blogger_service.GetNext(feed)
   f = open(''.join([dirname, 'feedinfo']), 'w')
   f.write(str(feed.title) + "\n")
   f.write(str(feed.author) + "\n")
   f.write(str(feed.id) + "\n")
+  f.close()
+  for entry in feed.entry:
+    saveToFile(entry, dirname)
+  feed = blogger_service.GetNext(feed)
   while feed is not None:
     for entry in feed.entry:
       saveToFile(entry, dirname)
@@ -47,29 +49,66 @@ def saveToFile(entry, dirname):
 # Takes all downloaded posts and creates local copies
 # of the referenced images.
 def sourceLocalImages(dirname, url):
-  netloc = urlparse(url).netloc
   # Writes to temp file in case of large files
   for file in os.listdir(dirname):
     f = open(os.path.join(dirname, file), 'r')
     print "opening:", file
     contentSoup = BeautifulStoneSoup(f.read())
     f.close()
-    imgTags = contentSoup.findAll('img')
-    for tag in imgTags:
-      # removes images referencing the blog itself
-      if netloc and netloc in tag['src']:
-        print "extracting:", tag
-        tag.extract()
-        continue
-      else:
-        print "orig tag src:", tag['src']
-        print "will replace image ref here"
+    replaceImageTags(contentSoup, url)
     tempfile = '.'.join([file, "tmp"])
     print "tempfile name:", tempfile
     ftemp = open(os.path.join(dirname, tempfile), 'w')
     ftemp.write(contentSoup.prettify())
     ftemp.close()
     os.rename(os.path.join(dirname, tempfile), os.path.join(dirname, file))
+
+# Replaces image tags contained in a link
+# with local copies of the images
+def replaceImageTags(contentSoup, url):
+  netloc = urlparse(url).netloc
+  # Replace links to images
+  links = contentSoup.findAll('a')
+  for link in links:
+    print "LINK:", link['href']
+    predType =  mimetypes.guess_type(link['href'])
+    if predType in ('image/png', 'image/jpeg', 'image/gif'):
+      print "+++DOWNLOADING:", link['href']
+      filename = download(link['href'])
+      link['href'] = filename
+  # Replace image tags
+  imgTags = contentSoup.findAll('img')
+  for tag in imgTags:
+    # removes images referencing the blog itself
+    if netloc and netloc in tag['src']:
+      tag.extract()
+      continue
+    else:
+      print "+++DOWNLOADING:", tag['src']
+      filename = download(tag['src'])
+      tag['src'] = filename
+
+def urlToName(url):
+  return basename(urlsplit(url)[2])
+
+def download(url, localFileName = None):
+  localName = urlToName(url)
+  req = urllib2.Request(url)
+  r = urllib2.urlopen(req)
+  if r.info().has_key('Content-Disposition'):
+    # If the response has a Content-Disposition the filename is taken from it
+    localName = r.info()['Content-Disposition'].split('filename=')[1]
+    if localName[0] == "" or localName[0] == "'":
+      localName = localName[1:-1]
+    elif r.url != url:
+      # A redirection occured, so take that filename from the final url
+      localName = urlToName(r.url)
+    if localFileName:
+      # Save the file under the user-specified name
+      localName = localFileName
+    f = open(localName, 'wb')
+    f.write(r.read())
+    f.close()
 
 def getUrl(filename):
   f = open(filename, 'r')
